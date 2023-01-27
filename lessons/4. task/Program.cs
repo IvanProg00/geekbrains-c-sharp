1d69f55b4218aa8b0ab1b01cdc8994bf27ae9ae0
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-01-27 18:54:19 +0100

    Create task 4

---
+++ lessons/4. task/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Enter a number: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n < 0)
+{
+	Console.WriteLine("Number must be equal or greater than 0");
+}
+else
+{
+	int count = -n;
+
+	while (count <= n)
+	{
+		Console.Write(count);
+		if (count < n)
+		{
+			Console.Write(", ");
+		}
+
+		count++;
+	}
+	Console.WriteLine();
+}
---
+++ lessons/4. task/README.md
@@ -0,0 +1,9 @@
+# 4 Task
+
+Write a program that takes one number (N) as input, and shows all integers in
+the range from -N to N as output.
+
+## Example
+
+- 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+- 2 -> " -2, -1, 0, 1, 2"
