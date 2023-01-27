b1ed06d765af44ebf3d91c3a2b5233c960c784b4
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-01-27 21:40:51 +0100

    Solve task 4

---
+++ homeworks/4. task/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Enter a number: ");
+int num = int.Parse(Console.ReadLine());
+
+int count = 2;
+
+while (count <= num)
+{
+	Console.Write(count);
+
+	if (count < num - 1)
+	{
+		Console.Write(", ");
+	}
+
+	count += 2;
+}
+
+Console.WriteLine();
