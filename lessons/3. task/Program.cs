cf1c1b970f0f9036f0ddbc5571ed885321230a71
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-01-27 18:46:40 +0100

    Create task 3

---
+++ lessons/3. task/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Enter the day of the week: ");
+int day = int.Parse(Console.ReadLine());
+
+if (day == 1)
+{
+	Console.WriteLine("Monday");
+}
+if (day == 2)
+{
+	Console.WriteLine("Tuesday");
+}
+if (day == 3)
+{
+	Console.WriteLine("Wednesday");
+}
+if (day == 4)
+{
+	Console.WriteLine("Thursday");
+}
+if (day == 5)
+{
+	Console.WriteLine("Friday");
+}
+if (day == 6)
+{
+	Console.WriteLine("Saturday");
+}
+if (day == 7)
+{
+	Console.WriteLine("Sunday");
+}
---
+++ lessons/3. task/README.md
@@ -0,0 +1,9 @@
+# 3 Task
+
+Write a program that will give out the name of the day of the week by a given
+number.
+
+## Example
+
+// 3 -> Wednesday
+// 5 -> Friday
