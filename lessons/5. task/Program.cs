e2d8b314af767b1dd66c1634d73a4c4caa283f02
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-01-27 19:05:40 +0100

    Create task 5

---
+++ lessons/5. task/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter a three-digit number: ");
+
+int num = int.Parse(Console.ReadLine());
+
+if (num > 999 || num < 100)
+{
+	Console.WriteLine("The number must contain 3 digits");
+}
+else
+{
+	int res = num % 10;
+	Console.WriteLine("Ответ: " + res);
+}
---
+++ lessons/5. task/README.md
@@ -0,0 +1,10 @@
+# 5 Task
+
+Write a program that accepts a three-digit number as input and shows the last
+digit of this number as output.
+
+## Example
+
+- 456 -> 6
+- 112 -> 2
+- 798 -> 8
