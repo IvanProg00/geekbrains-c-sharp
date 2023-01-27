0201b4a2a96e1cc594a9c80337f311fd5b4b4c11
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-01-27 19:41:54 +0100

    Implement homework task 1

---
+++ homeworks/1. task/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter first number: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+int num2 = int.Parse(Console.ReadLine());
+
+int min = num1,
+	max = num2;
+
+if (min > max)
+{
+	int v = min;
+	min = max;
+	max = v;
+}
+
+Console.WriteLine("The number " + min + " is less than " + max);
