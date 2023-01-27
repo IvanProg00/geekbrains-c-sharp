b666418da591673ae03b741457e9039e0dd61477
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-01-27 20:58:42 +0100

    Resolve homework task 2

---
+++ homeworks/2. task/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Enter first number: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter third number: ");
+int num3 = int.Parse(Console.ReadLine());
+
+int max = num1;
+
+if (num2 > max)
+{
+	max = num2;
+}
+
+if (num3 > max)
+{
+	max = num3;
+}
+
+Console.WriteLine("The maximum number is " + max);
