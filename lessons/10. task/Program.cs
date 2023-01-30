4ed40d2110ddb4c90c0a2b4100456ab928214e9d
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-01-30 20:57:03 +0100

    Implement lessons task 10

---
+++ lessons/10. task/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Enter first number: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 * num1 == num2)
+{
+	Console.WriteLine("Square of the first number is equal to second number");
+}
+else if (num2 * num2 == num1)
+{
+	Console.WriteLine("Square of the second number is equal to first number");
+}
+else
+{
+	Console.WriteLine("There is no number being square of another");
+}
---
+++ lessons/10. task/README.md
@@ -0,0 +1,11 @@
+# Task 10
+
+Write a program that takes two numbers as input and checks whether one number is
+the square of the other.
+
+## Example
+
+- 5, 25  ->  yes
+- -4, 16  ->  yes
+- 25, 5  ->  yes
+- 8,9  ->  no
