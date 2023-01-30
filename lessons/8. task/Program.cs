6c19ba60375fd6dfb675a184c91ac2d5f30951db
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-01-30 20:21:30 +0100

    Implement lessons task 8

---
+++ lessons/8. task/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Enter first number: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+int num2 = int.Parse(Console.ReadLine());
+
+int balance = num1 % num2;
+if (balance == 0)
+{
+	Console.WriteLine($"{num2} is a multiple of {num1}");
+}
+else
+{
+	Console.WriteLine($"{num2} is not a multiple of {num1}, reminder is {balance}");
+}
