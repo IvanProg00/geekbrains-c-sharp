9dc5cba931f79fcffc984655f44f912ad082c478
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-03 13:54:35 +0100

    Implement lesson task 12

---
+++ lessons/12. task/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Enter X: ");
+int x = int.Parse(Console.ReadLine());
+
+Console.Write("Enter Y: ");
+int y = int.Parse(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+	Console.WriteLine("I");
+}
+if (x < 0 && y > 0)
+{
+	Console.WriteLine("II");
+}
+if (x < 0 && y < 0)
+{
+	Console.WriteLine("III");
+}
+if (x > 0 && y < 0)
+{
+	Console.WriteLine("IV");
+}
---
+++ lessons/12. task/README.md
@@ -0,0 +1,5 @@
+# Task 11
+
+Write a program that takes the coordinates of a point (X and Y) as input, with
+X ≠ 0 and Y ≠ 0 and outputs the number of the quarter of the plane in which this
+point is located.
