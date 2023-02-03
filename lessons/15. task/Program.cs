d464de2702fadc6ae8dc7db85f8fe2d09eb2e307
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-03 14:23:35 +0100

    Implement lesson task 15

---
+++ lessons/15. task/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Enter number: ");
+int num = int.Parse(Console.ReadLine());
+
+int i = 1;
+
+while (i <= num)
+{
+	Console.Write(i*i);
+
+	if (i < num)
+	{
+		Console.Write(", ");
+	}
+
+	i++;
+}
+
+Console.WriteLine();
---
+++ lessons/15. task/README.md
@@ -0,0 +1,9 @@
+# Task 15
+
+Write a program that takes a number (N) as input and outputs a table of squares
+of numbers from 1 to N.
+
+## Example
+
+- 5 -> 1, 4, 9, 16, 25.
+- 2 -> 1,4
