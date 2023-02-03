f8d6f776aefb4e2cf03907e20052069226a1a590
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-03 14:39:11 +0100

    Implement homework task 8

---
+++ homeworks/8. task/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter 5 digit number: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num >= 100_000 || num < 10_000)
+{
+	Console.WriteLine("Number must contain 5 digits");
+	return;
+}
+
+if (num % 10 != num / 10_000 || num / 10 % 10 != num / 1000 % 10)
+{
+	Console.WriteLine("Not palindrome");
+}
+else
+{
+	Console.WriteLine("Palindrome");
+}
