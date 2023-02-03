2f2f8a9cb2f646b97fe46b75bc8856c6751f6d86
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-03 17:02:08 +0100

    Implement homework task 10

---
+++ homeworks/10. task/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter number: ");
+int num = int.Parse(Console.ReadLine());
+
+int i = 1;
+while (i <= num)
+{
+	Console.Write(Math.Pow(i, 3));
+
+	if (i < num)
+	{
+		Console.Write(", ");
+	}
+
+	i++;
+}
+
+Console.WriteLine();
