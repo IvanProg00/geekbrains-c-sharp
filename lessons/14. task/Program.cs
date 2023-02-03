66405963254e40ce5f32bd6b1b722bf6cae47100
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-03 14:12:26 +0100

    Implement lesson task 14

---
+++ lessons/14. task/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter coordinate A X: ");
+double ax = double.Parse(Console.ReadLine());
+Console.Write("Enter coordinate A Y: ");
+double ay = double.Parse(Console.ReadLine());
+
+
+Console.Write("Enter coordinate B X: ");
+double bx = double.Parse(Console.ReadLine());
+Console.Write("Enter coordinate B Y: ");
+double by = double.Parse(Console.ReadLine());
+
+double distance = Math.Sqrt(Math.Pow(ax-bx, 2) + Math.Pow(ay-by, 2));
+
+Console.WriteLine($"Distance between A and B is: {distance}");
---
+++ lessons/14. task/README.md
@@ -0,0 +1,9 @@
+# Task 14
+
+Write a program that takes the coordinates of two points as input and finds the
+distance between them in 2D space.
+
+## Example
+
+- `A (3, 6)`; `B (2, 1)` -> 5,09
+- `A (7, -5)`; `B (1, -1)` -> 7,21
