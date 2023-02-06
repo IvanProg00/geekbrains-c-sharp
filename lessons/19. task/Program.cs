6c69bdab6e32aff73a7ada00da9d04c735a1d47e
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-06 20:00:39 +0100

    Implement lesson task 19

---
+++ lessons/19. task/Program.cs
@@ -0,0 +1,43 @@
+﻿void FillRandomNums(int[] arr)
+{
+	Random rand = new Random();
+
+	for (int i = 0; i < arr.Length; i++)
+	{
+		arr[i] = rand.Next(0, 2);
+	}
+}
+
+int[] SumNums(int[] nums1, int[] nums2)
+{
+	int[] res = new int[nums1.Length];
+
+	for (int i = 0; i < nums1.Length; i++)
+	{
+		res[i] = nums1[i] + nums2[i];
+	}
+
+	return res;
+}
+
+void PrintNums(int[] arr)
+{
+	for (int i = 0; i < arr.Length; i++)
+	{
+		Console.Write(arr[i] + " ");
+	}
+
+	Console.WriteLine();
+}
+
+int[] nums1 = new int[8];
+int[] nums2 = new int[8];
+
+FillRandomNums(nums1);
+FillRandomNums(nums2);
+
+int[] numsSum = SumNums(nums1, nums2);
+
+PrintNums(nums1);
+PrintNums(nums2);
+PrintNums(numsSum);
---
+++ lessons/19. task/README.md
@@ -0,0 +1,9 @@
+# Task 19
+
+Write a program that outputs an array of 8 elements filled with zeros and ones in
+random order.
+
+## Example
+
+- `[1, 0, 1, 1, 0, 1, 0, 0]` + `[0, 0, 0, 0, 0, 0, 1, 0]` -> `[1, 1, 0, 0, 1, 0, 2, 1]`
+- `[0, 1, 0, 1, 1, 1, 1, 1]` + `[1, 0, 0, 1, 1, 1, 0, 1]` -> `[1, 1, 0, 2, 2, 2, 1, 2]`
