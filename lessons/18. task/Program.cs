f1ad1edafa9879142cba4e6f3a5f96561d48208b
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-06 19:56:12 +0100

    Implement lesson task 18

---
+++ lessons/18. task/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter number: ");
+int num = int.Parse(Console.ReadLine());
+
+int productNums = GetProductNums(num);
+Console.WriteLine($"Product of numbers from 1 to N {productNums}");
+
+int GetProductNums(int num)
+{
+	int res = 1;
+
+	for (int i = 1; i <= num; i++)
+	{
+		res *= i;
+	}
+
+	return res;
+}
---
+++ lessons/18. task/README.md
@@ -0,0 +1,9 @@
+# Task 18
+
+Write a program that takes the number N as input and outputs the product of
+numbers from 1 to N.
+
+## Example
+
+- 4 -> 24
+- 5 -> 120
