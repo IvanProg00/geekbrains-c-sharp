59aafe5b6badc941e0b341355c7af4ac6e96c33d
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-06 18:25:22 +0100

    Implement lesson task 16

---
+++ lessons/16. task/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter number: ");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Sum of numbers = {GetSumNums(num)}");
+
+int GetSumNums(int num)
+{
+	int res = 0;
+
+	while (num > 0)
+	{
+		res += num;
+		num--;
+	}
+
+	return res;
+}
---
+++ lessons/16. task/README.md
@@ -0,0 +1,10 @@
+# Task 16
+
+Write a program that accepts a number (A) as input and outputs the sum of
+numbers from 1 to A.
+
+## Example
+
+- 7 -> 28
+- 4 -> 10
+- 8 -> 36
