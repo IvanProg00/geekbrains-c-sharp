2fcd224cc6fb71674cf484cc2e0f5abb9841fc05
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-06 19:51:29 +0100

    Implement lesson task 17

---
+++ lessons/17. task/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Enter number: ");
+int num = int.Parse(Console.ReadLine());
+
+int numDigits = GetNumDigits(num);
+Console.WriteLine($"Number of digits {numDigits}");
+
+int GetNumDigits(int num)
+{
+	int res = 0;
+
+	while (num > 0)
+	{
+		num /= 10;
+		res++;
+	}
+
+	return res;
+}
---
+++ lessons/17. task/README.md
@@ -0,0 +1,8 @@
+# Task 17
+
+Write a program that accepts a number as input and outputs the number of digits
+in the number.
+
+- 456 -> 3
+- 78 -> 2
+- 89126 -> 5
