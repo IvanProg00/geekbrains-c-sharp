82b91b810df271327d2e4d5b03aa669e832c516f
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-07 18:41:54 +0100

    Implement lesson task 13

---
+++ homeworks/13. task/Program.cs
@@ -0,0 +1,28 @@
+﻿void FillRandomNums(int[] arr, int from, int to)
+{
+	Random rand = new Random();
+
+	for (int i = 0; i < arr.Length; i++)
+	{
+		arr[i] = rand.Next(from, to);
+	}
+}
+
+void PrintNums(int[] arr)
+{
+	for (int i = 0; i < arr.Length; i++)
+	{
+		Console.Write(arr[i]);
+
+		if (i < arr.Length - 1)
+		{
+			Console.Write(", ");
+		}
+	}
+
+	Console.WriteLine();
+}
+
+int[] arr = new int[8];
+FillRandomNums(arr, 1, 100);
+PrintNums(arr);
