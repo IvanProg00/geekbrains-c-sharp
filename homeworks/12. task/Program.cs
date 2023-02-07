7107bc5e1119950114e1d406494a1f3e65b14aad
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-07 17:59:38 +0100

    Implement lesson task 12

---
+++ homeworks/12. task/Program.cs
@@ -0,0 +1,18 @@
+﻿int SumDigits(int num)
+{
+	int res = 0;
+
+	while (num > 0)
+	{
+		res += num % 10;
+		num /= 10;
+	}
+
+	return res;
+}
+
+Console.Write("Enter number: ");
+int num = int.Parse(Console.ReadLine());
+
+int sumDigits = SumDigits(num);
+Console.WriteLine($"Sum of digits {sumDigits}");
---
+++ homeworks/12. task/README.md
@@ -0,0 +1,10 @@
+# Task 12
+
+Write a program that accepts a number as input and outputs the sum of the digits
+in the number.
+
+## Example
+
+- 452 -> 11
+- 82 -> 10
+- 9012 -> 12
