2b911f753d50c45b8f1bfd73c20b00da36836941
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-10 10:27:41 +0100

    Implement lesson task 22

---
+++ lessons/22. task/Program.cs
@@ -0,0 +1,51 @@
+﻿void FillRandomNumbers(int[] arr, int from, int to)
+{
+	Random rand = new Random();
+
+	for (int i = 0; i < arr.Length; i++)
+	{
+		arr[i] = rand.Next(from, to);
+	}
+}
+
+void PrintNums(int[] arr)
+{
+	foreach (int val in arr)
+	{
+		Console.Write(val + " ");
+	}
+	Console.WriteLine();
+}
+
+bool ElementExists(int[] arr, int val)
+{
+	foreach (int el in arr)
+	{
+		if (el == val)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+int[] arr = new int[5];
+int from = 0,
+	to = 20;
+
+FillRandomNumbers(arr, from, to);
+PrintNums(arr);
+
+int searchElement = new Random().Next(from, to);
+
+Console.WriteLine($"Search element {searchElement}");
+
+if (ElementExists(arr, searchElement))
+{
+	Console.WriteLine("Value exists");
+}
+else
+{
+	Console.WriteLine("Value not exists");
+}
---
+++ lessons/22. task/README.md
@@ -0,0 +1,8 @@
+# Task 22
+
+Write a program that determines whether a given number is present in an array.
+
+## Example
+
+- 4; `[6, 7, 19, 345, 3]` -> no
+- 3; `[6, 7, 19, 345, 3]` -> yes
