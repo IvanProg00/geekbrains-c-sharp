bc94a8e71b2d7516d937206240535975f03b77bf
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-10 10:15:35 +0100

    Implement lesson task 21

---
+++ lessons/21. task/Program.cs
@@ -0,0 +1,34 @@
+﻿void FillRandomNumber(int[] arr, int from, int to)
+{
+	Random rand = new Random();
+
+	for (int i = 0; i < arr.Length; i++)
+	{
+		arr[i] = rand.Next(from, to);
+	}
+}
+
+void ReverseNums(int[] arr)
+{
+	for (int i = 0; i < arr.Length; i++)
+	{
+		arr[i] *= -1;
+	}
+}
+
+void PrintNums(int[] arr)
+{
+	foreach (int el in arr)
+	{
+		Console.Write(el + " ");
+	}
+
+	Console.WriteLine();
+}
+
+int[] arr = new int[4];
+FillRandomNumber(arr, -9, 10);
+
+PrintNums(arr);
+ReverseNums(arr);
+PrintNums(arr);
---
+++ lessons/21. task/README.md
@@ -0,0 +1,8 @@
+# Task 21
+
+Write a program to replace array elements. Replace the positive elements with the
+corresponding negative ones, and vice versa.
+
+## Example
+
+- `[-4, -8, 8, 2]` -> `[4, 8, -8, -2]`
