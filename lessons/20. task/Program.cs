edb17dd54e6ce95d194a32a28616195dff8ee569
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-10 10:00:24 +0100

    Implement lesson task 20

---
+++ lessons/20. task/Program.cs
@@ -0,0 +1,28 @@
+﻿void FillRandomNumbers(int[] arr, int from, int to)
+{
+	Random rand = new Random();
+
+	for (int i = 0; i < arr.Length; i++)
+	{
+		arr[i] = rand.Next(from, to);
+	}
+}
+
+int[] arr = new int[12];
+FillRandomNumbers(arr, -9, 10);
+
+int positiveSum = 0, negativeSum = 0;
+
+foreach (int el in arr)
+{
+	if (el > 0)
+	{
+		positiveSum += el;
+	}
+	else if (el < 0)
+	{
+		negativeSum += el;
+	}
+}
+
+Console.WriteLine($"Sum of positive numbers = {positiveSum}, sum of negative numbers = {negativeSum}");
---
+++ lessons/20. task/README.md
@@ -0,0 +1,8 @@
+# Task 20
+
+Specify an array of 12 elements filled with random numbers from the interval
+`[-9, 9]`. Find the sum of the negative and positive elements of the array.
+
+## Example
+
+- [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] -> sum of positive numbers 29, sum of negative numbers -20.
