258bcf65f513597092b6ab029758c77a03719867
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-11 13:25:06 +0100

    Implement lesson task 24

---
+++ lessons/24. task/Program.cs
@@ -0,0 +1,46 @@
+﻿void FillRandomNumbers(int[] arr, int from, int to)
+{
+	Random rand = new Random();
+
+	for (int i = 0; i < arr.Length; i++)
+	{
+		arr[i] = rand.Next(from, to);
+	}
+}
+
+void PrintNums(int[] arr)
+{
+	foreach (int el in arr)
+	{
+		Console.Write(el + " ");
+	}
+	Console.WriteLine();
+}
+
+int[] ProductArray(int[] arr)
+{
+	int midSize = (arr.Length + 1) / 2;
+	int[] res = new int[midSize];
+
+	for (int i = 0; i < midSize; i++)
+	{
+		int reversePos = arr.Length - i - 1;
+		if (i != reversePos)
+		{
+			res[i] = arr[i] * arr[reversePos];
+		}
+		else
+		{
+			res[i] = arr[i];
+		}
+	}
+
+	return res;
+}
+
+int[] arr = new int[5];
+FillRandomNumbers(arr, 0, 20);
+PrintNums(arr);
+
+int[] result = ProductArray(arr);
+PrintNums(result);
---
+++ lessons/24. task/README.md
@@ -0,0 +1,10 @@
+# Task 24
+
+Find the product of pairs of numbers in a one-dimensional array. We consider the
+first and last element as a pair, the second and penultimate, etc. Write the
+result in a new array.
+
+## Example
+
+- `[1, 2, 3, 4, 5]` -> 5 8 3
+- `[6, 7, 3, 6]` -> 36 21
