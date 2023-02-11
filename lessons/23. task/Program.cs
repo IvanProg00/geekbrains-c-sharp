2e2e7f534549ea286ad976cf62c0ad31e5031d6e
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-11 13:22:12 +0100

    Implement lesson task 23

---
+++ lessons/23. task/Program.cs
@@ -0,0 +1,44 @@
+﻿void FillRandomNumbers(int[] arr, int from, int to)
+{
+	Random rand = new Random();
+
+	for (int i = 0; i < arr.Length; i++)
+	{
+		arr[i] = rand.Next(from, to);
+	}
+}
+
+void PrintNumbers(int[] arr)
+{
+	foreach (int el in arr)
+	{
+		Console.Write(el + " ");
+	}
+	Console.WriteLine();
+}
+
+int CountFromTo(int[] arr, int from, int to)
+{
+	int count = 0;
+
+	foreach (int val in arr)
+	{
+		if (val >= from && val < to)
+		{
+			count++;
+		}
+	}
+
+	return count;
+}
+
+int[] arr = new int[1];
+
+FillRandomNumbers(arr, 0, 200);
+PrintNumbers(arr);
+
+int from = 10,
+	to = 100;
+int countFromTo = CountFromTo(arr, from, to);
+
+Console.WriteLine($"Чисел с {from}, до {to} = {countFromTo}");
---
+++ lessons/23. task/README.md
@@ -0,0 +1,12 @@
+# Task 23
+
+Set a one-dimensional array of 123 random numbers. Find the number of array
+elements whose values lie in the segment `[10, 99]`.
+
+## Example
+
+Example for an array of 5, not 123 elements. In your solution, do for 123.
+
+- `[5, 18, 123, 6, 2]` -> 1
+- `[1, 2, 3, 6, 2]` -> 0
+- `[10, 11, 12, 13, 14]` -> 5
