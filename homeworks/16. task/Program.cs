0b5e82c53b8b2af5a4fd660913d6c0d0ca25615a
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-11 14:40:06 +0100

    Implement homework task 16

---
+++ homeworks/16. task/Program.cs
@@ -0,0 +1,53 @@
+﻿void FillRandomNumbers(int[] arr, int from, int to)
+{
+	Random rand = new Random();
+
+	for (int i = 0; i < arr.Length; i++)
+	{
+		arr[i] = rand.Next(from, to);
+	}
+}
+
+void PrintNumbers(int[] arr)
+{
+	foreach (int val in arr)
+	{
+		Console.Write(val + " ");
+	}
+	Console.WriteLine();
+}
+
+int FindDifferenceMinAndMax(int[] arr)
+{
+	// If the array size is less than 2, it will return the value -1.
+	// Because we can not find minimum and maximum.
+	if (arr.Length < 2)
+	{
+		return -1;
+	}
+
+	int min = arr[0],
+		max = arr[0];
+
+	for (int i = 1; i < arr.Length; i++)
+	{
+		if (min > arr[i])
+		{
+			min = arr[i];
+		}
+
+		if (max < arr[i])
+		{
+			max = arr[i];
+		}
+	}
+
+	return max - min;
+}
+
+int[] arr = new int[6];
+FillRandomNumbers(arr, 0, 100);
+PrintNumbers(arr);
+
+int diff = FindDifferenceMinAndMax(arr);
+Console.WriteLine($"Difference between minimum and maximum is {diff}");
---
+++ homeworks/16. task/README.md
@@ -0,0 +1,8 @@
+# Task 16
+
+Specify an array of real numbers. Find the difference between the maximum and
+minimum elements of the array.
+
+## Example
+
+- `[3, 7, 22, 2, 78]` -> 76
