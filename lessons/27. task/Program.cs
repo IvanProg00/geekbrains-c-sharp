bf8cffb5310d19627133ba839b6f036076d186c9
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-13 22:04:03 +0100

    Implement lesson task 27

---
+++ lessons/27. task/Program.cs
@@ -0,0 +1,22 @@
+﻿int AskNumber(string msg)
+{
+	Console.Write(msg);
+	return int.Parse(Console.ReadLine());
+}
+
+string IntoBinary(int num)
+{
+	string res = String.Empty;
+
+	while (num > 0)
+	{
+		res = num % 2 + res;
+		num /= 2;
+	}
+
+	return res;
+}
+
+int num = AskNumber("Enter number: ");
+string binary = IntoBinary(num);
+Console.WriteLine(binary);
---
+++ lessons/27. task/README.md
@@ -0,0 +1,9 @@
+# Task 27
+
+Write a program that will convert a decimal number to binary.
+
+## Example
+
+- 45 -> 101101
+- 3 -> 11
+- 2 -> 10
