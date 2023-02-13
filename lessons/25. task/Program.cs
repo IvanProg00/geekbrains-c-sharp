81f2a84422f8b0c6a915ec0c35b4441de55384bc
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-13 18:41:12 +0100

    Implement lesson task 25

---
+++ lessons/25. task/Program.cs
@@ -0,0 +1,52 @@
+﻿void FillRandomNumbers(int[] arr, int minValue, int maxValue)
+{
+	Random rand = new Random();
+	for (int i = 0; i < arr.Length; i++)
+	{
+		arr[i] = rand.Next(minValue, maxValue);
+	}
+}
+
+void PrintNumbers(int[] arr)
+{
+	Console.WriteLine(String.Join(", ", arr));
+}
+
+void ReverseArray1(int[] arr)
+{
+	for (int i = 0; i < arr.Length / 2; i++)
+	{
+		int reversePos = arr.Length - i - 1;
+
+		int temp = arr[i];
+		arr[i] = arr[reversePos];
+		arr[reversePos] = temp;
+	}
+}
+
+int[] ReverseArray2(int[] arr)
+{
+	int[] res = new int[arr.Length];
+
+	for (int i = 0; i < arr.Length; i++)
+	{
+		res[i] = arr[arr.Length - 1 - i];
+	}
+
+	return res;
+}
+
+int[] arr1 = new int[5];
+FillRandomNumbers(arr1, 0, 11);
+Console.WriteLine("Reverse array method 1");
+PrintNumbers(arr1);
+ReverseArray1(arr1);
+PrintNumbers(arr1);
+
+int[] arr2 = new int[5];
+FillRandomNumbers(arr2, 0, 11);
+Console.WriteLine("Reverse array method 2");
+PrintNumbers(arr2);
+int[] reverseArr = ReverseArray2(arr2);
+PrintNumbers(reverseArr);
+
---
+++ lessons/25. task/README.md
@@ -0,0 +1,9 @@
+# Task 25
+
+Write a program that will flip a one-dimensional array (the last element will be
+in the first place, and the first one will be in the last, etc.).
+
+## Example
+
+- `[1, 2, 3, 4, 5]` -> `[5, 4, 3, 2, 1]`
+- `[6, 7, 3, 6]` -> `[6, 3, 7, 6]`
