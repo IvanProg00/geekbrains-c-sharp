70755e0694b12c14da9d15b328b65171599838d8
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-13 22:08:28 +0100

    Implement lesson task 28

---
+++ lessons/28. task/Program.cs
@@ -0,0 +1,36 @@
+﻿int AskNumber(string msg)
+{
+	Console.Write(msg);
+	return int.Parse(Console.ReadLine());
+}
+
+int[] Fibonacci(int n)
+{
+	int prev = 0,
+		curr = 1;
+
+	int[] res = new int[n];
+
+	if (n >= 1)
+	{
+		res[0] = prev;
+	}
+	if (n >= 2)
+	{
+		res[1] = curr;
+	}
+
+	for (int i = 2; i < n; i++)
+	{
+		int temp = curr;
+		curr += prev;
+		prev = temp;
+
+		res[i] = curr;
+	}
+	return res;
+}
+
+int num = AskNumber("Enter number: ");
+int[] fib = Fibonacci(num);
+Console.WriteLine(String.Join(" ", fib));
---
+++ lessons/28. task/README.md
@@ -0,0 +1,10 @@
+# Task 28
+
+Without using recursion, output the first N Fibonacci numbers. The first two
+Fibonacci numbers are 0 and 1.
+
+## Example
+
+- 5 -> 0 1 1 2 3
+- 3 -> 0 1 1
+- 7 -> 0 1 1 2 3 5 8
