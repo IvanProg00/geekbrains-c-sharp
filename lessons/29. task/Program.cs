d5d21aa25ebb1ba1e1738da7cc991e4c31c1f28a
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-13 22:11:57 +0100

    Implement lesson task 29

---
+++ lessons/29. task/Program.cs
@@ -0,0 +1,31 @@
+﻿void FillRandomNumbers(int[] arr, int minValue, int maxValue)
+{
+	Random rand = new Random();
+	for (int i = 0; i < arr.Length; i++)
+	{
+		arr[i] = rand.Next(minValue, maxValue);
+	}
+}
+
+void PrintNumbers(int[] arr)
+{
+	Console.WriteLine(String.Join(", ", arr));
+}
+
+int[] CopyArray(int[] arr)
+{
+	int[] res = new int[arr.Length];
+
+	for (int i = 0; i < arr.Length; i++)
+	{
+		res[i] = arr[i];
+	}
+
+	return res;
+}
+
+int[] arr1 = new int[5];
+FillRandomNumbers(arr1, 0, 11);
+PrintNumbers(arr1);
+int[] arr2 = CopyArray(arr1);
+PrintNumbers(arr2);
---
+++ lessons/29. task/README.md
@@ -0,0 +1,3 @@
+# Task 29
+
+Write a program that will create a copy of a given array using piecemeal copying.
