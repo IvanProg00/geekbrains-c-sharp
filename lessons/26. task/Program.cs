f642138148c053fff9173111eb69b55f84054e57
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-13 22:01:28 +0100

    Implement lesson task 26

---
+++ lessons/26. task/Program.cs
@@ -0,0 +1,23 @@
+﻿int AskNumber(string msg)
+{
+	Console.Write(msg);
+	return int.Parse(Console.ReadLine());
+}
+
+bool IsTriangleValid(int side1, int side2, int side3)
+{
+	return side2 + side3 > side1 && side1 + side3 > side2 && side1 + side2 > side3;
+}
+
+int side1 = AskNumber("Enter length of the first side: ");
+int side2 = AskNumber("Enter length of the second side: ");
+int side3 = AskNumber("Enter length of the third side: ");
+
+if (IsTriangleValid(side1, side2, side3))
+{
+	Console.WriteLine("Triangle is valid");
+}
+else
+{
+	Console.WriteLine("Triangle sides sizes are incorrect");
+}
---
+++ lessons/26. task/README.md
@@ -0,0 +1,7 @@
+# Task 26
+
+Write a program that accepts three numbers as input and checks whether a triangle
+with sides of this length can exist.
+
+Triangle inequality theorem: each side of a triangle is less than the sum of the
+other two sides.
