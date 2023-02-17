802ef2aefc732b06f4e4f2ea0e53f39dc5d0a0af
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-17 16:59:37 +0100

    Implement lessons task 33

---
+++ lessons/33. task/Program.cs
@@ -0,0 +1,56 @@
+﻿int AskNumber(string msg)
+{
+	Console.Write(msg);
+	return int.Parse(Console.ReadLine());
+}
+
+void FillArray(int[,] arr, int minValue, int maxValue)
+{
+	Random rand = new Random();
+
+	for (int i = 0; i < arr.GetLength(0); i++)
+	{
+		for (int j = 0; j < arr.GetLength(1); j++)
+		{
+			arr[i, j] = rand.Next(minValue, maxValue);
+		}
+	}
+}
+
+void PrintNumbers(int[,] arr)
+{
+	for (int i = 0; i < arr.GetLength(0); i++)
+	{
+		for (int j = 0; j < arr.GetLength(1); j++)
+		{
+			Console.Write(arr[i, j] + " ");
+		}
+		Console.WriteLine();
+	}
+}
+
+int SumDiagonalNumbers(int[,] arr)
+{
+	int res = 0;
+	int pos = 0;
+
+	while (pos < arr.GetLength(0) && pos < arr.GetLength(1))
+	{
+		res += arr[pos, pos];
+		pos++;
+	}
+
+	return res;
+}
+
+int rows = AskNumber("Enter rows: ");
+int cols = AskNumber("Enter columns: ");
+int minValue = AskNumber("Enter minimum random value: ");
+int maxValue = AskNumber("Enter maximum random value: ");
+
+int[,] arr = new int[rows, cols];
+
+FillArray(arr, minValue, maxValue);
+PrintNumbers(arr);
+int sumNums = SumDiagonalNumbers(arr);
+Console.WriteLine("Sum of the numbers on the main diagonal = " + sumNums);
---
+++ lessons/33. task/README.md
@@ -0,0 +1,14 @@
+# Task 33
+
+Specify a two-dimensional array. Find the sum of the elements located on the main
+diagonal (with indexes `[0, 0]`, `[1, 1]` and etc).
+
+## Example
+
+- ```plain
+  1 4 7 2
+  5 9 2 3
+  8 4 2 4
+  ```
+
+  The result -> 1+9+2 = 12
