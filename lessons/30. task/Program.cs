fe9613c214ee00d88a9114149394b645a599c925
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-17 16:47:29 +0100

    Implement lessons task 30

---
+++ lessons/30. task/Program.cs
@@ -0,0 +1,37 @@
+﻿int AskNumber(string msg)
+{
+	Console.Write(msg);
+	return int.Parse(Console.ReadLine());
+}
+
+void FillRandomNumbers(int[,] arr, int minValue, int maxValue)
+{
+	Random rand = new Random();
+
+	for (int i = 0; i < arr.GetLength(0); i++)
+	{
+		for (int j = 0; j < arr.GetLength(1); j++)
+		{
+			arr[i, j] = rand.Next(minValue, maxValue);
+		}
+	}
+}
+
+void PrintNumbers(int[,] arr)
+{
+	for (int i = 0; i < arr.GetLength(0); i++)
+	{
+		for (int j = 0; j < arr.GetLength(1); j++)
+		{
+			Console.Write(arr[i, j] + " ");
+		}
+		Console.WriteLine();
+	}
+}
+
+int rows = AskNumber("Enter number of rows: ");
+int cols = AskNumber("Enter number of columns: ");
+
+int[,] arr = new int[rows, cols];
+FillRandomNumbers(arr, 0, 10);
+PrintNumbers(arr);
---
+++ lessons/30. task/README.md
@@ -0,0 +1,13 @@
+# Task 30
+
+Specify a two-dimensional array of size `m×n` filled with random integers.
+
+## Example
+
+- `m = 3`, `n = 4`.
+
+  ```plain
+  1 4 8 19
+  5 -2 33 -2
+  77 3 8 1
+  ```
