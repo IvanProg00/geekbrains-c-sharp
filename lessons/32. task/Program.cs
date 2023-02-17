37d37d253198c97db37ba993fe2d5a80c0786dfa
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-17 16:53:41 +0100

    Implement lessons task 32

---
+++ lessons/32. task/Program.cs
@@ -0,0 +1,50 @@
+﻿int AskNumber(string msg)
+{
+	Console.Write(msg);
+	return int.Parse(Console.ReadLine());
+}
+
+void FillArray(int[,] arr)
+{
+	for (int i = 0; i < arr.GetLength(0); i++)
+	{
+		for (int j = 0; j < arr.GetLength(1); j++)
+		{
+			arr[i, j] = i + j;
+		}
+	}
+}
+
+void PrintNumbers(int[,] arr)
+{
+	for (int i = 0; i < arr.GetLength(0); i++)
+	{
+		for (int j = 0; j < arr.GetLength(1); j++)
+		{
+			Console.Write(arr[i, j] + " ");
+		}
+		Console.WriteLine();
+	}
+}
+
+void PowEvenNumbers(int[,] arr)
+{
+	for (int i = 0; i < arr.GetLength(0); i += 2)
+	{
+		for (int j = 0; j < arr.GetLength(1); j += 2)
+		{
+			arr[i, j] = arr[i, j] * arr[i, j];
+		}
+	}
+}
+
+int rows = AskNumber("Enter rows: ");
+int cols = AskNumber("Enter columns: ");
+
+int[,] arr = new int[rows, cols];
+
+FillArray(arr);
+PrintNumbers(arr);
+Console.WriteLine("----------");
+PowEvenNumbers(arr);
+PrintNumbers(arr);
---
+++ lessons/32. task/README.md
@@ -0,0 +1,4 @@
+# Task 32
+
+Specify a two-dimensional array. Find the elements that have both indexes even,
+and replace these elements with their squares.
