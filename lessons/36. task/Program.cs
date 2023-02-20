8b022e5449bbac983c1bb80ab69a78dfdcc0ae07
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-20 20:42:19 +0100

    Implement lessons task 36

---
+++ lessons/36. task/Program.cs
@@ -0,0 +1,60 @@
+﻿int AskNumber(string msg)
+{
+	Console.Write(msg);
+	return int.Parse(Console.ReadLine());
+}
+
+void FillArray(int[,] arr, int minValue, int maxValue)
+{
+	Random rand = new Random();
+
+	for (int i = 0; i < arr.GetLength(0); i++)
+	{
+		for (int j = 0; j < arr.GetLength(1); j++)
+		{
+			arr[i, j] = rand.Next(minValue, maxValue);
+		}
+	}
+}
+
+void PrintNumbers(int[,] arr)
+{
+	for (int i = 0; i < arr.GetLength(0); i++)
+	{
+		for (int j = 0; j < arr.GetLength(1); j++)
+		{
+			Console.Write(arr[i, j] + " ");
+		}
+		Console.WriteLine();
+	}
+}
+
+int[,] FlipArray(int[,] arr)
+{
+	if (arr.GetLength(0) == 0 || arr.GetLength(1) == 0)
+	{
+		return new int[0, 0];
+	}
+
+	int[,] res = new int[arr.GetLength(1), arr.GetLength(0)];
+
+	for (int i = 0; i < arr.GetLength(0); i++)
+	{
+		for (int j = 0; j < arr.GetLength(1); j++)
+		{
+			res[j, i] = arr[i, j];
+		}
+	}
+
+	return res;
+}
+
+int rows = AskNumber("Enter number of rows: ");
+int cols = AskNumber("Enter number of columns: ");
+
+int[,] arr = new int[rows, cols];
+FillArray(arr, 0, 20);
+PrintNumbers(arr);
+Console.WriteLine("__________");
+int[,] arrFlip = FlipArray(arr);
+PrintNumbers(arrFlip);
---
+++ lessons/36. task/README.md
@@ -0,0 +1,3 @@
+# Task 36
+
+Specify a two-dimensional array. Write a program that replaces rows with columns.
