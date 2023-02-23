d6c1f560d7e6010ab66d9e2474d570604b9a6a9f
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-23 22:39:39 +0100

    Implement lesson task 41

---
+++ lessons/41. task/Program.cs
@@ -0,0 +1,26 @@
+﻿int AskNumber(string msg)
+{
+	Console.Write(msg);
+	return int.Parse(Console.ReadLine());
+}
+
+int Pow(int num, int degree)
+{
+	if (degree <= 0)
+	{
+		return 1;
+	}
+
+	if (degree == 1)
+	{
+		return num;
+	}
+
+	return num * Pow(num, --degree);
+}
+
+int num = AskNumber("Enter number: ");
+int degree = AskNumber("Enter degree: ");
+
+int res = Pow(num, degree);
+Console.WriteLine(res);
