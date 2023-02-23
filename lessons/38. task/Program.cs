ad23be08ffee4bae580952b8bcf22726c4b89b89
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-23 22:25:03 +0100

    Implement lesson task 38

---
+++ lessons/38. task/Program.cs
@@ -0,0 +1,26 @@
+﻿int AskNumber(string msg)
+{
+	Console.Write(msg);
+	return int.Parse(Console.ReadLine());
+}
+
+void PrintNaturalNumber(int toNum, int num = 1)
+{
+	if (num <= toNum)
+	{
+		Console.Write(num);
+		if (num < toNum)
+		{
+			Console.Write(", ");
+		}
+
+		PrintNaturalNumber(toNum, ++num);
+	}
+	else
+	{
+		Console.WriteLine();
+	}
+}
+
+int num = AskNumber("Enter number: ");
+PrintNaturalNumber(num);
---
+++ lessons/38. task/README.md
@@ -0,0 +1,9 @@
+# Task 38
+
+Set the value to N. Write a program that outputs all natural numbers in the range
+from 1 to N.
+
+## Example
+
+- `N = 5` -> "1, 2, 3, 4, 5"
+- `N = 6` -> "1, 2, 3, 4, 5, 6"
