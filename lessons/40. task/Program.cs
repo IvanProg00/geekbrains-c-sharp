9cb7a25353126eaf173227d265b66df38c4fdb71
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-23 22:36:22 +0100

    Implement lesson task 40

---
+++ lessons/40. task/Program.cs
@@ -0,0 +1,20 @@
+﻿int AskNumber(string msg)
+{
+	Console.Write(msg);
+	return int.Parse(Console.ReadLine());
+}
+
+int SumNumbers(int num)
+{
+	if (num <= 0)
+	{
+		return num;
+	}
+
+	return num % 10 + SumNumbers(num / 10);
+}
+
+int num = AskNumber("Enter number: ");
+
+int res = SumNumbers(num);
+Console.WriteLine(res);
---
+++ lessons/40. task/README.md
@@ -0,0 +1,9 @@
+# Task 40
+
+Write a program that will accept a number as input and return the sum of its
+digits.
+
+## Example
+
+- 453 -> 12
+- 45 -> 9
