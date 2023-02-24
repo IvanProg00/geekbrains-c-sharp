53df69bb7a22e151a08a1a27bca1cd0ae994bfe5
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-02-24 11:44:10 +0100

    Complete homework task 28

---
+++ homeworks/28. task/Program.cs
@@ -0,0 +1,20 @@
+﻿int AskNumber(string msg)
+{
+	Console.Write(msg);
+	return int.Parse(Console.ReadLine());
+}
+
+int NumbersRangeSum(int from, int to)
+{
+	if (from > to)
+	{
+		return 0;
+	}
+
+	return from + NumbersRangeSum(from + 1, to);
+}
+
+int from = AskNumber("Enter first number: ");
+int to = AskNumber("Enter second number: ");
+int numsSum = NumbersRangeSum(from, to);
+Console.WriteLine($"Sum of numbers from {from} to {to} = {numsSum}");
